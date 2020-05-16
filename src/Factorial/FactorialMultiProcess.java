564916c9099ac78b9cecc1f95e297a279dcbae64
hamzasalah <hamzasalah@gmail.com>
2020-05-15 18:36:00 -0700

    added comments

--- src/Factorial/FactorialMultiProcess.java
+++ src/Factorial/FactorialMultiProcess.java
@@ -4,24 +4,43 @@ import javax.xml.stream.FactoryConfigurationError;
 import java.math.BigInteger;
 
 public class FactorialMultiProcess extends Thread{
+    //Number we are interested in getting the factorial of
     public BigInteger num;
+    //limit to calculate partial factorials
     public BigInteger limit;
+    //Storing answers
     public BigInteger answer;
 
 
+    /**
+     *
+     * @param num used to find a partial factorial of any number - only used for multithreaded case
+     * @param limit is the limit to which we want to calculate the partial factorial to
+     */
     public FactorialMultiProcess(BigInteger num, BigInteger limit){
         this.num = num;
         this.limit = limit;
     }
 
+    /**
+     *
+     * @param number of which we want to calculate the factorial of
+     * @return the factorial of the number
+     */
     public static BigInteger factorialRecursive(BigInteger number){
 
         if (number.compareTo(BigInteger.ONE) <= 0)
             return BigInteger.ONE;
-        else // recursion step
+        else
             return number.multiply(factorialRecursive(number.subtract(BigInteger.ONE)));
     }
 
+    /**
+     *
+     * @param number the number of which we want to find the partial factorial of using the limit.
+     *               This number we use in this case is the one with which we instantiate the object with
+     * @return partial factorial of a number based on the limit
+     */
     public BigInteger factorialRecursivePartial(BigInteger number){
 
         if (number.compareTo(limit) <= 0)
@@ -30,6 +49,9 @@ public class FactorialMultiProcess extends Thread{
             return number.multiply(factorialRecursivePartial(number.subtract(BigInteger.ONE)));
     }
 
+    /**
+     * Method that starts our threads. Here we store our partial factorials in the answer variable
+     */
     public void run() {
 
         answer = factorialRecursivePartial(num);
--- src/Factorial/main.java
+++ src/Factorial/main.java
@@ -9,61 +9,86 @@ public class main {
     public static void main(String args[])
     {
 
+        //Note in this case the 11999 does not matter since we can get the factorial of any number.
         FactorialMultiProcess bigInt = new FactorialMultiProcess(BigInteger.valueOf(11999),BigInteger.valueOf(1));
 
+        //Start time to calculate the time it takes to run the entire process
         Date prePureRecursive =  new Date();
+
+        //Taking the factorial of any number using recursion
         BigInteger resultPureRecusive = bigInt.factorialRecursive(BigInteger.valueOf(12200));
+
+        //End time of the entire process
         Date postPureRecursive = new Date();
 
+        //Our factorial output
         System.out.println("The result is: " + resultPureRecusive);
+
+        //The total time it takes to process the factorial
         System.out.println(" and it took " + (postPureRecursive.getTime() - prePureRecursive.getTime()) + " milliseconds to compute using the recursive method.");
 
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////MULTITHREADED FACTORIAL/////////
 
-
+        //We initiate the number we want to calculate the factorial of
         BigInteger number = BigInteger.valueOf(12200);
+        //Set our lowest boundary
         BigInteger absoluteLowest = BigInteger.ONE;
+        //Set our midpoint
         BigInteger midPoint =  number.divide(BigInteger.TWO);
+        //Set our lowest quarter which is the midpoint between the lowest boundary and our original midpoint
         BigInteger lowerHalfMidPoint = midPoint.divide(BigInteger.TWO);
+        //Set our highest quarter which is the midpoint between our original mid point and our number
         BigInteger upperQuarterUpper = lowerHalfMidPoint.multiply(BigInteger.valueOf(3));
 
+        //Initializing our variable for our final result will be our final result
         BigInteger result = BigInteger.ZERO;
 
 
-
+        //Start time of our entire process
         Date pre = new Date();
 
+        //Instantiate object that will calculate partial factorial from lowest boundary to the lowest quarter
         FactorialMultiProcess fmp1 = new FactorialMultiProcess(lowerHalfMidPoint, absoluteLowest);
+        //Instantiate object that will calculate partial factorial from lowest quarter to midpoint
         FactorialMultiProcess fmp2 = new FactorialMultiProcess(midPoint, lowerHalfMidPoint.add(BigInteger.ONE));
+        //Instantiate object that will calculate partial factorial from midpoint to highest quarter
         FactorialMultiProcess fmp3 = new FactorialMultiProcess(upperQuarterUpper, midPoint.add(BigInteger.ONE));
+        //Instantiate object that will calculate partial factorial from our highest quarter to our number
         FactorialMultiProcess fmp4 = new FactorialMultiProcess(number, upperQuarterUpper.add(BigInteger.ONE));
 
         try {
 
 
-
+            //Starting our threads where the four partial factorials will be calculated together
             fmp1.start();
             fmp2.start();
             fmp3.start();
             fmp4.start();
 
+            //Once the process completes, we join the threads to our main thread
             fmp1.join();
             fmp2.join();
             fmp3.join();
             fmp4.join();
 
 
+            //Each partial factorial object stores their partial factorial in a variable called answer.
+            //We then retrieve the partial answers and multiply them together to get the full factorial
             result = fmp1.answer.multiply(fmp2.answer.multiply(fmp3.answer.multiply(fmp4.answer)));
 
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
+        //Ent time of our entire process
         Date post = new Date();
 
+        //Printing result
         System.out.println("The result is: " + result);
 
+        //Printing the time it took to complete the process
         System.out.println(" and it took " + (post.getTime() - pre.getTime()) + " milliseconds to compute using threads.");
 
     }
